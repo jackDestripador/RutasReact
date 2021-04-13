a5123cae2aff8e9118ba6397726dba96a13efa08
jesus <chucho.cisneros@hotmail.com>
2021-04-13 10:39:56 -0500

    UsseEffect
    uso del concepto usse Effect que tiene que ver con el ciclo del componente

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
 const Base = () => {
+  const [nombre,setNombre] =useState('Pedro')
+  useEffect(() => {
+    setTimeout(() => {
+      setNombre('Jesús');
+    },2000)
+    
+  })
   return (
     <div>
       <h1>Pagina de Base Rutas/ </h1>
+      {nombre}
     </div>
     );
 }
