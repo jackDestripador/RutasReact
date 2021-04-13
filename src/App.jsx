305b03759a5713b4daedd8cdf5cc252374edb9c7
jesus <chucho.cisneros@hotmail.com>
2021-04-13 11:18:48 -0500

    Parametros de rutas
    parametros de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Inicio from './components/inicio'
+import Inicio from './components/Inicio'
 import Base from './components/Base'
 import {
   BrowserRouter as Router,
@@ -14,12 +14,14 @@ function App() {
       <Link to="/inicio">Inicio</Link>
       <Link to="/">Base</Link>
       <Switch>
+         <Route path="/inicio/:id/:nombre/:edad">
+          <Inicio/>
+        </Route>
         <Route exact path="/">
           <Base/>
         </Route>
-        <Route path="/inicio">
-          <Inicio/>
-        </Route>
+
+       
         
       </Switch>
       
